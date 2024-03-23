513a7e629de622c37c842d02343f54d584f93651
jdiyorbek <diyorbekjurabekovich16092005@gmail.com>
2024-03-23 11:07:33 +0500

    AdminChecker is fixed

--- src/routes/library.js
+++ src/routes/library.js
@@ -3,8 +3,8 @@ const adminChecker = require("../middleware/adminChecker");
 const {publishBook, getAll, getById, deleteById} = require("../Controllers/libraryController");
 
 router.post("/", adminChecker, publishBook)
-router.get("/", adminChecker, getAll)
-router.get("/:id", adminChecker, getById)
+router.get("/", getAll)
+router.get("/:id", getById)
 router.delete("/:id", adminChecker, deleteById)
 
 module.exports = router
